39f3117a66c70c22760c66144ae0a46334281e17
Jesse Pinho <jesse@jessepinho.com>
2020-01-14 14:43:34 +0100

    Configure Contentful and upgrade to latest libraries

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: "j66qheg22uli",
+        accessToken: "04QAYaRsr548atM4KwInDCWk8v84agx0uS7r8LrQutk",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
